951f6f036499539c1b29458032c7dcbf092c71af
a.sikora <artsikora1@gmail.com>
2024-11-24 11:02:58 +0100

    Theme setup

--- app/src/main/java/com/npaw/dailynotes/ui/theme/Type.kt
+++ app/src/main/java/com/npaw/dailynotes/ui/theme/Type.kt
@@ -1,34 +1,54 @@
 package com.npaw.dailynotes.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+import com.npaw.dailynotes.R.font
+
+
+val openSans = FontFamily(
+    Font(
+        resId = font.open_sans_bold
+                ,weight = FontWeight.Bold,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    Font(
+        resId = font.open_sans_regular,
+        weight = FontWeight.Normal,
+
+    ),
+    Font(
+        resId = font.open_sans_etra_bold,
+        weight = FontWeight.ExtraBold
+    ),
+    Font(
+        resId = font.open_sans_light,
+        weight = FontWeight.Light,
+
+    ),
 )
+
+
+
+// Default Material 3 typography values
+val baseline = Typography()
+
+val Typography = Typography(
+    displayLarge = baseline.displayLarge.copy(fontFamily = openSans),
+    displayMedium = baseline.displayMedium.copy(fontFamily = openSans),
+    displaySmall = baseline.displaySmall.copy(fontFamily = openSans),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = openSans),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = openSans),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = openSans),
+    titleLarge = baseline.titleLarge.copy(fontFamily = openSans),
+    titleMedium = baseline.titleMedium.copy(fontFamily = openSans),
+    titleSmall = baseline.titleSmall.copy(fontFamily = openSans),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = openSans),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = openSans),
+    bodySmall = baseline.bodySmall.copy(fontFamily = openSans),
+    labelLarge = baseline.labelLarge.copy(fontFamily = openSans),
+    labelMedium = baseline.labelMedium.copy(fontFamily = openSans),
+    labelSmall = baseline.labelSmall.copy(fontFamily = openSans),
+)
+
